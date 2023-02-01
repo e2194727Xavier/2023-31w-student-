084932d249fb21028977534a44579d6b006fece7
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-02-01 10:52:29 -0500

    cours 2 ajout de get_header et get_footer et la début  boucle wordpress

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<h1>Bonjour tout le monde, bienvenu sur mon site</h1>
+<!-- modele index.php représente le modele par défaut  -->
+<?php get_header();?><!-- Il va récuperer le header -->
+<main>
+    <h3>index.php</h3>
+<?php if (have_posts()):
+    while(have_posts()): the_post();
+    the_title('<h1>', '</h1>');
+    the_content();
+    endwhile;
+endif;
+?>
+</main>
+<?get_footer();?>
