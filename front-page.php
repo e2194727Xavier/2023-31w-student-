438c151faf1fba5b21d7e03de6f48c35b5567514
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-02-17 07:52:59 -0500

    ajout de section et article dans front-page et ajout blockflex dans composant

--- front-page.php
+++ front-page.php
@@ -13,11 +13,11 @@
                 //the_excerpt();
         ?>
                 <article>
-                <h3><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h3>
+                    <h3><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h3>
 
-                <p><?php echo wp_trim_words(get_the_excerpt(), 4); ?></p> <!-- Le nombre de mot que je veux après la premiere virgule, le troisieme arguement est optionnel  -->
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 4); ?></p> <!-- Le nombre de mot que je veux après la premiere virgule, le troisieme arguement est optionnel  -->
                 </article>
-        <?php endwhile;?>
+            <?php endwhile; ?>
         <?php endif;
         ?>
     </section>
