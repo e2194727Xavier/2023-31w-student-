edaf86dcabf358bb322d879fc77f634fe5bfcdc3
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-02-17 07:49:28 -0500

    ajout de section et article dans front-page et ajout blockflex dans composant

--- front-page.php
+++ front-page.php
@@ -1,20 +1,25 @@
 <!-- modele index.php représente le modele par défaut  -->
-<?php get_header();?><!-- Il va récuperer le header -->
+<?php get_header(); ?><!-- Il va récuperer le header -->
 <main>
     <code>front-page.php</code>
     <h3>index.php</h3>
-<?php if (have_posts()):
-        while(have_posts()): the_post();
-            //the_title('<h1>', '</h1>');
-            //the_permalink();?>
+    <section class="blockflex">
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post();
+                //the_title('<h1>', '</h1>');
+                //the_permalink();
+                /* ============== */
+                //the_content();
+                //the_excerpt();
+        ?>
+                <article>
+                <h3><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h3>
 
-            <h1><a href="<?php the_permalink();?>"><?=get_the_title();?></a></h1>
-            <?php
-            //the_content();
-            //the_excerpt();
-            echo wp_trim_words(get_the_excerpt(),4);/* Le nombre de mot que je veux après la premiere virgule, le troisieme arguement est optionnel */
-        endwhile;
-endif;
-?>
+                <p><?php echo wp_trim_words(get_the_excerpt(), 4); ?></p> <!-- Le nombre de mot que je veux après la premiere virgule, le troisieme arguement est optionnel  -->
+                </article>
+        <?php endwhile;?>
+        <?php endif;
+        ?>
+    </section>
 </main>
-<?php get_footer();?>
+<?php get_footer(); ?>
