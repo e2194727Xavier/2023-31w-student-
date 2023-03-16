462587bbad856f3d803bf0890964c1d4a1d6ae79
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-03-16 15:37:25 -0400

    c14 header

--- header.php
+++ header.php
@@ -1,38 +1,10 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@e2194727Xavier 
-eddytuto
-/
-2023-31w
-Public
-Fork your own copy of eddytuto/2023-31w
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-2023-31w/header.php /
-@eddypmartin
-eddypmartin c14 menu entete debut burger
-Latest commit c2b24b8 1 minute ago
- History
- 1 contributor
-35 lines (34 sloc)  1.18 KB
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="X to the Z" >
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
@@ -61,4 +33,3 @@ Latest commit c2b24b8 1 minute ago
        get_template_part("template-parts/aside");
     }
      ?>
-    
