091a19490ff5498aba8d1bb3322d00d27cbb4dd7
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-03-19 18:20:17 -0400

    Ajout de style dans la page et modification du template parts categorie-note-wp

--- template-parts/categorie-note-wp.php
+++ template-parts/categorie-note-wp.php
@@ -6,7 +6,11 @@
 */
 
 $titre = get_the_title();
-/* if(substr($titre, 0,1) === "0") */
+if(substr($titre, 0,2) === "00"){
+    $titre = substr($titre, 3);
+}else if(substr($titre, 0,1) === "0"){
+    $titre = substr($titre, 1);
+}
 ?>
 <article class="blocflex__article">
     <h5><a href="<?php the_permalink(); ?>"> <?= $titre; ?></a></h5>
