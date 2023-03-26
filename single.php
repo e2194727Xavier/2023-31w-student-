e30496fe1e6fbc6b21e46b15ddd08fd55f3b0cd8
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-03-26 17:49:34 -0400

    Modification du markdown

--- single.php
+++ single.php
@@ -5,16 +5,20 @@
 get_header() ?>
 <main class="site__main">
     <h3>Single.php</h3>
-<?php 
-if (have_posts()):
-   while(have_posts()): the_post();
-        the_title('<h1>','</h1>');
-        the_content(); 
-        /* in_category() */
-        the_field('enseignant');
-    endwhile;    
-endif;
-?>   
+    <?php 
+    if (have_posts()):
+        while(have_posts()): the_post();
+            the_title('<h1>','</h1>');
+            the_content(); 
+            /* in_category() */
+            $enseignant = get_field('enseignant');
+            if ($enseignant) {
+                echo '<p>Enseignant : ' . $enseignant . '</p>';
+            }
+        endwhile;    
+    endif;
+    ?>   
 </main> 
 <?php get_footer(); ?>
+
 
