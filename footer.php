07ff01b4164386f718f4d7bea1b0252256f260a3
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-04-02 23:39:11 -0400

    ajout de style et le menu fonctionne maintenant

--- footer.php
+++ footer.php
@@ -1,24 +1,26 @@
 <?php
+
 /**
-* Template footer.php
-*/
+ * Template footer.php
+ */
 ?>
 <footer class="site__footer">
-<section class="footer__widget">   
-    <div><?php dynamic_sidebar( 'pied-page-1' ); ?></div>
-    <div><?php dynamic_sidebar( 'pied-page-2' ); ?></div>
-    <div><?php dynamic_sidebar( 'pied-page-3' ); ?></div>
-</section> 
-<section class="footer__lien">
-    <div><?php wp_nav_menu(array(
-                            'menu'=>'lien-externe-1',
-                          'container'=>'nav',
-                         )); ?></div>
-    <div><?php ?></div>
-    <div><?php ?></div>
-</section>
+    <section class="footer__widget">
+        <div><?php dynamic_sidebar('pied-page-1'); ?></div>
+        <div><?php dynamic_sidebar('pied-page-2'); ?></div>
+        <div><?php dynamic_sidebar('pied-page-3'); ?></div>
+    </section>
+    <section class="footer__lien">
+        <div><?php wp_nav_menu(array(
+                    "menu" => "lien",
+                    "container" => "nav",
+                    "container-class" => "footer-nav"
+                )); ?></div>
+    </section>
+    <div class="footer__infoPerso">
+        <?php the_custom_logo(); ?>
+    </div>
 
 </footer>
 <?php wp_footer(); ?>
 </body>
-</html>
