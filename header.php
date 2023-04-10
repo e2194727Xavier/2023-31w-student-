54b673fcb97f9a07fa7f58a2e3d66872f4db3dc3
e2194727Xavier <e2194727@cmaisonneuve.qc.ca>
2023-04-10 19:31:21 -0400

    mettre le texte au centre dans le menu secondaire d'evenement

--- header.php
+++ header.php
@@ -3,9 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-PzUGIyBXXexeiZJW8VnU6H5U6ZuQLlHfW7J75rNR+5r5VUK+NKam8OQWZ/w9OZ3NayjK1yiD59l//LMJhRyGA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">d
     <meta author="X to the Z" >
     <title>Document</title>
     <?php wp_head(); ?>
